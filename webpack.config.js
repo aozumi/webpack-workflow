06cfa3d5312a97d3a65fc1b2aff42df28be1ba3e
aozumi <68194841+aozumi@users.noreply.github.com>
2020-11-08 18:18:55 +0900

    Basic Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,14 @@
+const path = require('path');
+
 module.exports = {
+    mode: 'development',
     devServer: { port: 3000 },
+
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '',
+    },
+    devtool: 'eval-cheap-module-source-map',
 }
